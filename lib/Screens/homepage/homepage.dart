d8ffbd9a95784452432b7c68243d863d5af8f33e
Swarnika Suman <68819451+SwarnikaSuman@users.noreply.github.com>
2020-10-03 20:34:39 +0530

    TextStyle

--- lib/Screens/homepage/homepage.dart
+++ lib/Screens/homepage/homepage.dart
@@ -3,6 +3,6 @@ import 'package:flutter/widgets.dart';
 class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(child:Center(child:Text("Home Page")));
+    return Container(child:Center(child:Text("Home Page", style: TextStyle())));
   }
 }
